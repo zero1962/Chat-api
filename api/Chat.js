26c4bcf4820b9d23e93f49076d968b909c6c8c39
zero1962 <GitHub>
2025-09-07 07:19:10 +0000

    Add Gemini webhook endpoint

--- api/Chat.js
+++ api/Chat.js
@@ -1,4 +1,4 @@
-// api/chat.js
+// api/chat00.js
 export default async function handler(req, res) {
   // CORSヘッダーを追加
   res.setHeader("Access-Control-Allow-Origin", "http://zero1962b.world.coocan.jp");
