fc82e59b0405cb783826bdf5d6ae5cf733499a0a
zero1962 <kuniyuki.tsujimoto1182@nifty.com>
2025-09-10 14:07:04 +0000

    変更記録の説明コメント

--- api/webhook.js
+++ api/webhook.js
@@ -1,5 +1,5 @@
 // api/webhook.js　2025.09.10（Vercel対応・ESM形式）
-console.log("🫧 Webhookバージョン: 2025.09.10-23:15");
+console.log("🫧 Webhookバージョン: 2025.09.10-23:06");
 import { callGeminiAPI } from '../copakopa-webhook/callGeminiAPI.js'; // 拡張子 .js を忘れずに！
 
 const geminiIntents = [
